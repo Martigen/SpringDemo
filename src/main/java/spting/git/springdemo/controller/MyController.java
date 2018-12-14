2af374015a6a338fc7dd466bc4dc1b38448556a1
Mateusz <mateusz.matusik@poczta.fm>
2018-12-14 14:09:07 +0100

    [change,add] zmiana mycontroler + dodanie you.html

--- src/main/java/spting/git/springdemo/controller/MyController.java
+++ src/main/java/spting/git/springdemo/controller/MyController.java
@@ -1,6 +1,7 @@
 package spting.git.springdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +10,13 @@ public class MyController {
     @GetMapping({"/","/home"})
     public String welcome(){
         return "welcome";
+    }
+
+    @GetMapping("/you")
+    public String you(Model model){
+        model.addAttribute("on","to on");
+        model.addAttribute("jego","to jego");
+        return "you";
     }
 
 }
